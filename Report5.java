973b5b8825489eadd6bd4b5bb0af8b755bd6d189
Taro Asahina <e205713@ie.u-ryukyu.ac.jp>
2020-12-19 11:23:08 +0900

    2nd commit

--- Report5.java
+++ Report5.java
@@ -1,6 +1,13 @@
 class Report5{
     public static void main(String[] args) {
-        String str = null;
-        str.length();
+        String str = null;    
+        try {
+            System.out.println("受け取った文字列は" + str + "です。");
+            System.out.println("文字列の長さは" + str.length() + "です。");
+        } catch (NullPointerException e) {
+            //TODO: handle exception
+            System.out.println("例外が発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
